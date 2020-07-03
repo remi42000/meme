1d5a98c14574c14c7bf8a9ed8a4acd74f6a3b39a
remi42000 <rgdu42@yandex.com>
2020-07-03 22:45:01 +0200

    add question

---
+++ js/quiz.js
@@ -0,0 +1,31 @@
+// MEME: Who is the more innocent person on happy server ?
+
+let questions = [
+    "Who is the more innocent person on happy server ?",
+    "Who is the best chief cook on the server ?",
+    "Who the worst singer in karaoke channel"
+];
+
+let possibleAnswerQ1 = [
+    "a) remi",
+    "b) koffeee",
+    "c) melon",
+    "d) sophie",
+];
+
+let possibleAnswerQ2 = [
+    "a) Bob",
+    "b) koffeee",
+    "c) melon",
+    "d) sophie",
+];
+
+let possibleAnswerQ3 = [
+    "a) remi",
+    "b) koffeee",
+    "c) melon",
+    "d) Bob",
+];
+
+//score
+let score = 0;
