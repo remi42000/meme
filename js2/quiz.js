50b71d1b7e325a0032e7dbb45ed0c96bab9401c6
remi42000 <rgdu42@yandex.com>
2020-07-31 22:54:48 +0200

    new things simple

--- js2/quiz.js
+++ js2/quiz.js
@@ -1,34 +1,36 @@
+let btnREMI = document.getElementById("REMI");
 // MEME: Who is the more innocent person on happy server ?
 
-let questions = [
-    "Who is the more innocent person on happy server ?",
-    "Who is the best chief cook on the server ?",
-    "Who the worst singer in karaoke channel"
-];
+// let questions = [
+//     "Who is the more innocent person on happy server ?",
+//     "Who is the best chief cook on the server ?",
+//     "Who the worst singer in karaoke channel"
+// ];
 
-let possibleAnswerQ1 = [
-    "a) remi",
-    "b) koffeee",
-    "c) melon",
-    "d) sophie",
-];
+// let possibleAnswerQ1 = [
+//     "a) remi",
+//     "b) koffeee",
+//     "c) melon",
+//     "d) sophie",
+// ];
 
-let possibleAnswerQ2 = [
-    "a) Bob",
-    "b) koffeee",
-    "c) melon",
-    "d) sophie",
-];
+// let possibleAnswerQ2 = [
+//     "a) Bob",
+//     "b) koffeee",
+//     "c) melon",
+//     "d) sophie",
+// ];
 
-let possibleAnswerQ3 = [
-    "a) remi",
-    "b) koffeee",
-    "c) melon",
-    "d) Bob",
-];
-
-
+// let possibleAnswerQ3 = [
+//     "a) remi",
+//     "b) koffeee",
+//     "c) melon",
+//     "d) Bob",
+// ];
 
 
-//score
-let score = 0;
+
+
+// //score
+// let score = 0;
+btnREMI.addEventListener("click", function() { alert}, false);
